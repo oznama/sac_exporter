b53d6c1fa174174f452c0ea1cb821e358d63e949
Oziel Naranjo <oziel.naranjo@sas-mexico.com>
2023-04-28 16:48:18 -0600

    Rename component 07 to 11

--- src/com/prosa/obj/ReportSetting.java
+++ src/com/prosa/obj/ReportSetting.java
@@ -47,11 +47,11 @@ public class ReportSetting {
 		ReportConfig repConfig = new ReportConfig();
 		repConfig.setReportName(args[0]);
 		repConfig.setPdfName(args[1]);
-		repConfig.getParams().put(ReportParams.BANCO, args[2]);
-		repConfig.getParams().put(ReportParams.TIPO_LIQUIDACION, args[3]);
-		repConfig.getParams().put(ReportParams.ROLE, args[4]);
-		repConfig.getParams().put(ReportParams.RANGO_FECHA_INI, args[5]);
-		repConfig.getParams().put(ReportParams.RANGO_FECHA_FIN, args[6]);
+//		repConfig.getParams().put(ReportParams.BANCO, args[2]);
+		repConfig.getParams().put(ReportParams.TIPO_LIQUIDACION, args[2]);
+		repConfig.getParams().put(ReportParams.ROLE, args[3]);
+		repConfig.getParams().put(ReportParams.RANGO_FECHA_INI, args[4]);
+		repConfig.getParams().put(ReportParams.RANGO_FECHA_FIN, args[5]);
 		return repConfig;
 	}
 
